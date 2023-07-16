5e343deb35e96c4389f16a89eb808ff3fdfc8d67
solomon-wamutu <solomonwamutu16@gmail.com>
2023-07-17 00:36:30 +0300

    modified admin reset pass

--- admin/pages_reset_pwd.php
+++ admin/pages_reset_pwd.php
@@ -11,5 +11,37 @@ else{
     $err = "Enter your email";
 }
 
-if(!filter)
+if(!filter_list($_POST['email'],FILTER_VALIDATE_EMAIL)){
+$err = 'Invalid email';
+}
+
+$checkEmail = mysqli_query($mysqli,"SELECT `email` FROM `ib_admn` WHERE `email = '" . $_POST["email"] . "'");
+if (mysqli_num_rows($checkEmail) > 0) {
+$n = date('Y');
+$new_password = bin2hex(random_bytes($n));
+$upd = "UPDATE ib_admin SET password = ? WHERE email =?";
+$stmt = $mysqli->prepare($upd);
+$rc = $stmt->bind_param('ss', $new_password, $email);
+$stmt->execute();
+$_SESSION['email'] = $email;
+
+if($stmt) {
+    $success = "Confirm Your Password" && header("refresh:1; url=pages_confirm_passwords.php");
+}
+else{
+    $err = "Password reset failed";
+}
+} 
+else {
+        $err = "Email does not exist";
+    }
+
+    $sel = "SELECT * FROM `ib_systemsettings";
+    $stmt = $mysqli->prepare($sel);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    while($auth = $res->fetch_object()){
+
+    }
+
 }
