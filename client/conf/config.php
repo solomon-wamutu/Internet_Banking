2843f504ce872bdea147b3be72e429947b22c3c2
solomon-wamutu <solomonwamutu16@gmail.com>
2023-08-03 00:08:40 +0300

    modified

--- client/conf/config.php
+++ client/conf/config.php
@@ -4,9 +4,3 @@ $dbpass = "";
 $dbhost = "localhost";
 $db = "onlinebank";
 $mysqli = new mysqli($dbhost,$dbuser,$dbpass,$db);
-
-// if ($mysqli->connect_error) {
-//     die("Connection failed: " . $mysqli->connect_error);
-// }
-
-// echo "Connected successfully";
