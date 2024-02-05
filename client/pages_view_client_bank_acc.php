39ab53decbb3cbc3e7810df025f5b29e64f7490f
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-05 23:56:40 +0300

    client account

--- client/pages_view_client_bank_acc.php
+++ client/pages_view_client_bank_acc.php
@@ -1,8 +1,18 @@
-<?php 
+<?php
 session_start();
 include('./conf/config.php');
 include('./conf/checklogin.php');
 check_login();
-$client_id
+$client_id = $_SESSION['client_id'];
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<?php include("dist/_partials/head.php"); ?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
+    <div class="wrapper">
+        <?php include("dist/_partials/nav.php") ?>
+        <?php include("dist/_partials/sidebar.php") ?>
