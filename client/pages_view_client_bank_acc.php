6d4a73b1d50696f1da8ddce8810cbb574aa8c044
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-06 00:32:28 +0300

    client account

--- client/pages_view_client_bank_acc.php
+++ client/pages_view_client_bank_acc.php
@@ -27,63 +27,85 @@ $client_id = $_SESSION['client_id'];
         $cnt = 1;
         while ($row = $res->fetch_object()) {
         ?>
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1><?php echo $row->name; ?> iBanking Accounts</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="pages_dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="pages_balance_enquiries.php">Finances</a></li>
-                                <li class="breadcrumb-item"><a href="pages_balance_enquiries.php">Balances</a></li>
-                                <li class="breadcrumb-item active"><?php echo $row->name; ?> Accs</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Select on any action options to check your account balances</h3>
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1><?php echo $row->name; ?> iBanking Accounts</h1>
                             </div>
-                            <div class="card-body">
-                                <table id="example1" class="table table-hover table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Acc Number</th>
-                                            <th>Rate</th>
-                                            <th>Acc Type</th>
-                                            <th>Acc Owner</th>
-                                            <th>Date Opened</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="pages_dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="pages_balance_enquiries.php">Finances</a></li>
+                                    <li class="breadcrumb-item"><a href="pages_balance_enquiries.php">Balances</a></li>
+                                    <li class="breadcrumb-item active"><?php echo $row->name; ?> Accs</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
 
-                                    <?php
-                                    $client_id = $_SESSION['client_id'];
-                                    $sel = "SELECT * FROM ib_bankaccounts WHERE account_id =?";
-                                    $stmt = $mysqli->prepare($sel);
-                                    $stmt->bind_param('i', $account_id);
-                                    $stmt->execute();
-                                    $res = $stmt->get_result();
-                                    $cnt = 1;
-                                    while($row = $res->fetch_object()){
-                                        
-                                    }
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Select on any action options to check your account balances</h3>
+                                </div>
+                                <div class="card-body">
+                                    <table id="example1" class="table table-hover table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Acc Number</th>
+                                                <th>Rate</th>
+                                                <th>Acc Type</th>
+                                                <th>Acc Owner</th>
+                                                <th>Date Opened</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                                    ?>
+                                            <?php
+                                            $client_id = $_SESSION['client_id'];
+                                            $sel = "SELECT * FROM ib_bankaccounts WHERE account_id =?";
+                                            $stmt = $mysqli->prepare($sel);
+                                            $stmt->bind_param('i', $account_id);
+                                            $stmt->execute();
+                                            $res = $stmt->get_result();
+                                            $cnt = 1;
+                                            while ($row = $res->fetch_object()) {
+                                                $dateOpened = $row->created_at;
+
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $cnt; ?></td>
+                                                    <td><?php echo $row->acc_name; ?></td>
+                                                    <td><?php echo $row->account_number; ?></td>
+                                                    <td><?php echo $row->acc_rates; ?>%</td>
+                                                    <td><?php echo $row->acc_type; ?></td>
+                                                    <td><?php echo $row->client_name; ?></td>
+                                                    <td><?php echo date("d-M-Y", strtotime($dateOpened)); ?></td>
+                                                    <td>
+                                                        <a class="btn btn-success btn-sm" href="pages_check_client_acc_balance.php?account_id=<?php echo $row->account_id; ?>&acccount_number=<?php echo $row->account_number; ?>">
+                                                            <i class="fas fa-eye"></i>
+                                                            <i class="fas fa-money-bill-alt"></i>
+                                                            Check Balance
+                                                        </a>
+
+                                                    </td>
+
+                                                </tr>
+                                            <?php
+                                            $cnt = $cnt + 1;
+                                            }
+                                            ?>
+
                                         <?php
-                                                }
-?>
+                                    }
+                                        ?>
