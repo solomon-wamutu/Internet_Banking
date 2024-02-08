7d8467dae71af11e17800e5820e86138860947e1
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-08 23:31:55 +0300

    finan withdrawal

--- client/pages_financial_reporting_withdrawals.php
+++ client/pages_financial_reporting_withdrawals.php
@@ -5,3 +5,21 @@ include('conf/checklogin.php');
 check_login();
 $client_id = $_SESSION['client_id'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<?php include("dist/_partials/head.php"); ?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
+    <div class="wrapper">
+        <?php include("dist/_partials/nav.php"); ?>
+        <?php include("dist/_partials/sidebar.php"); ?>
+    </div>
+
+</body>
+
+</html>
