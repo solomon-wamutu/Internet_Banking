d216ebb4c1c620d7c3bc83195f5d88d94d504ffb
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-09 18:07:10 +0300

    finan reporting

--- client/pages_financial_reporting_transfers.php
+++ client/pages_financial_reporting_transfers.php
@@ -58,17 +58,17 @@ $client_id = $_SESSION['client_id'];
                                     </thead>
                                     <tbody>
                                         <?php
-                                          $client_id = $_SESSION['client_id'];
-                                          $sel = "SELECT * FROM ib_transactions WHERE tr_type = 'Transfer'AND client_id = ?";
-                                          $stmt = $mysqli->prepare($sel);  
-                                          $stmt->bind_param('i',$client_id);
-                                          $stmt->execute();
-                                          $res = $stmt -> get_result();
-                                          $cnt = 1;
-                                          while($row = $res->fetch_object()){
+                                        $client_id = $_SESSION['client_id'];
+                                        $sel = "SELECT * FROM ib_transactions WHERE tr_type = 'Transfer'AND client_id = ?";
+                                        $stmt = $mysqli->prepare($sel);
+                                        $stmt->bind_param('i', $client_id);
+                                        $stmt->execute();
+                                        $res = $stmt->get_result();
+                                        $cnt = 1;
+                                        while ($row = $res->fetch_object()) {
                                             $transTstamp = $row->created_at;
 
-                                            if($row->tr_type == 'Deposit'){
+                                            if ($row->tr_type == 'Deposit') {
                                                 $alertClass = "<span class='badge badge-success'>$row->tr_type</span>";
                                             } elseif ($row->tr_type == 'Withdrawal') {
                                                 $alertClass = "<span class='badge badge-danger'>$row->tr_type</span>";
@@ -76,9 +76,32 @@ $client_id = $_SESSION['client_id'];
                                                 $alertClass = "<span class='badge badge-warning'>$row->tr_type</span>";
                                             }
                                         ?>
-                                         
 
+                                            <tr>
+                                                <td><?php echo $cnt  ?></td>
+                                                <td><?php echo $row->tr_code; ?></td>
+                                                <td><?php echo $row->account_number  ?></td>
+                                                <td><?php echo $row->transaction_amt  ?></td>
+                                                <td><?php echo $row->client_name  ?></td>
+                                                <td><?php echo $row->receiving_acc_no  ?></td>
+                                                <td><?php echo $row->receiving_acc_holder   ?></td>
+                                                <td><?php echo date("d-M-Y h:m:s ", strtotime($transTstamp));   ?></td>
+                                            </tr>
 
-                                         <?php
-                                          }
-                                          ?>
+                                        <?php
+                                            $cnt = $cnt + 1;
+                                        }
+                                        ?>
+                                        </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
+        </div>
