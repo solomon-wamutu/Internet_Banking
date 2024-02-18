a8218a1946e358fed8c14c355e3eb54ed5d866bf
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-18 23:33:29 +0300

    add accounts

--- client/pages_add_acc_type.php
+++ client/pages_add_acc_type.php
@@ -5,6 +5,10 @@ include('./conf/checklogin.php');
 check_login();
 $admin_id = $_SESSION['admin_id'];
 if(isset($_POST['create_acc_type'])){
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $rate = $_POST['rate'];
+    $code = $_POST['code'];
     
 }
 ?>
