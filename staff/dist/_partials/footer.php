f1c9e05a4c8ab3886a1f76efd56f0492f8b06e57
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-06 23:54:38 +0300

    footer

---
+++ staff/dist/_partials/footer.php
@@ -0,0 +1,16 @@
+<?php
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `ib_systemsettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+  <footer class="main-footer">
+    <strong>&copy; 2020-<?php echo date('Y'); ?></strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 2.0.0
+    </div>
+  </footer>
+<?php } ?>
