6d3ab202c2173fdcbe1fab0cd3b69ea7c5aa2c62
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-18 12:39:29 +0300

    append

--- staff/dist/_partials/footer.php
+++ staff/dist/_partials/footer.php
@@ -7,7 +7,7 @@ $res = $stmt->get_result();
 while ($sys = $res->fetch_object()) {
 ?>
   <footer class="main-footer">
-    <strong>&copy; 2020-<?php echo date('Y'); ?></strong>
+    <strong>&copy; 2023-<?php echo date('Y'); ?></strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 2.0.0
