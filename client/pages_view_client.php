784dfd06c1b1c2bc90d4db3c744c26e3907ef3aa
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-27 01:00:00 +0300

    client view

---
+++ client/pages_view_client.php
@@ -0,0 +1,10 @@
+<?php
+include("admin/conf/config.php");
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `ib_systemsettings` ";
+// iB_SystemSettings
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
