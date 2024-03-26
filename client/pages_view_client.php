085304c327e961a5966b8e39a5b0ef3bbb1c337e
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-27 01:37:46 +0300

    client view

--- client/pages_view_client.php
+++ client/pages_view_client.php
@@ -297,3 +297,27 @@ if (isset($_POST['update_client_account'])) {
             <?php
             }
             ?>
+
+        </div>
+        <!-- /.content-wrapper -->
+        <?php include("dist/_partials/footer.php"); ?>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="dist/js/demo.js"></script>
+</body>
+
+</html>
